2529fee97f4b996fc929cd0b95c26f14308a84f4
prasanth-selva <prasanthselvaraj1511@gmail.com>
2026-04-19 17:18:17 +0530

    publication section

--- src/components/ResearchSection.tsx
+++ src/components/ResearchSection.tsx
@@ -55,7 +55,7 @@ const cardVariants = {
   visible: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.7, ease: [0.16, 1, 0.3, 1] },
+    transition: { duration: 0.7, ease: [0.16, 1, 0.3, 1] as const },
   },
 };
 
